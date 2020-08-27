0ee6b66ac057e32653e1c803ecd7430b86b55a33
heygum <47473616+heygum@users.noreply.github.com>
2020-08-27 18:38:41 +1000

    add week04 codefirst

---
+++ Week04/FIT5032_W04_CodeFirst/Controllers/studentsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FIT5032_W04_CodeFirst.DAL;
+using FIT5032_W04_CodeFirst.Models;
+
+namespace FIT5032_W04_CodeFirst.Controllers
+{
+    public class studentsController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        // GET: students
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+        // GET: students/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            students students = db.Students.Find(id);
+            if (students == null)
+            {
+                return HttpNotFound();
+            }
+            return View(students);
+        }
+
+        // GET: students/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: students/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,LastName,FirstMidName")] students students)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(students);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(students);
+        }
+
+        // GET: students/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            students students = db.Students.Find(id);
+            if (students == null)
+            {
+                return HttpNotFound();
+            }
+            return View(students);
+        }
+
+        // POST: students/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,LastName,FirstMidName")] students students)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(students).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(students);
+        }
+
+        // GET: students/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            students students = db.Students.Find(id);
+            if (students == null)
+            {
+                return HttpNotFound();
+            }
+            return View(students);
+        }
+
+        // POST: students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            students students = db.Students.Find(id);
+            db.Students.Remove(students);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Week04/FIT5032_W04_CodeFirst/DAL/StudentContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FIT5032_W04_CodeFirst.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace FIT5032_W04_CodeFirst.DAL
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<students> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
