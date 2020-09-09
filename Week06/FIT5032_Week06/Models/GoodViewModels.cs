1ad87b97cd8e0c83e376af368abb36c2ea334922
heygum <47473616+heygum@users.noreply.github.com>
2020-09-09 10:06:56 +1000

    week06

---
+++ Week06/FIT5032_Week06/Models/GoodViewModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FIT5032_Week06.Models
+{
+    public class GoodViewModels
+    {
+    }
+
+    public class GoodFormViewModel
+    {
+        [Required]
+        [Display(Name = "Good Name")]
+        public string GoodName { get; set; }
+        public int GoodNumber { get; set; }
+    }
+}
