f1f1aca73382d8cafc7a147b353baa0aa56966eb
heygum <47473616+heygum@users.noreply.github.com>
2020-09-18 15:51:05 +1000

    FIT5032_V0.3
    Rebuild the MVC architecture.

---
+++ FIT5032_AssignmentX/Controllers/MovePlansController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FIT5032_AssignmentX.Models;
+
+namespace FIT5032_AssignmentX.Controllers
+{
+    public class MovePlansController : Controller
+    {
+        private MovesContainer db = new MovesContainer();
+
+        // GET: MovePlans
+        public ActionResult Index()
+        {
+            return View(db.MovePlans.ToList());
+        }
+
+        // GET: MovePlans/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MovePlans movePlans = db.MovePlans.Find(id);
+            if (movePlans == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movePlans);
+        }
+
+        // GET: MovePlans/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MovePlans/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PlanId,MoveName,Time,Round,Date")] MovePlans movePlans)
+        {
+            if (ModelState.IsValid)
+            {
+
+                db.MovePlans.Add(movePlans);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(movePlans);
+        }
+
+        // GET: MovePlans/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MovePlans movePlans = db.MovePlans.Find(id);
+            if (movePlans == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movePlans);
+        }
+
+        // POST: MovePlans/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PlanId,MoveName,Time,Round,Date")] MovePlans movePlans)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movePlans).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movePlans);
+        }
+
+        // GET: MovePlans/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MovePlans movePlans = db.MovePlans.Find(id);
+            if (movePlans == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movePlans);
+        }
+
+        // POST: MovePlans/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MovePlans movePlans = db.MovePlans.Find(id);
+            db.MovePlans.Remove(movePlans);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FIT5032_AssignmentX/Controllers/MovementsController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FIT5032_AssignmentX.Models;
+
+namespace FIT5032_AssignmentX.Controllers
+{
+    [Authorize (Roles ="admin")]
+    public class MovementsController : Controller
+    {
+        private MovesContainer db = new MovesContainer();
+
+        // GET: Movements
+        public ActionResult Index()
+        {
+            return View(db.Movements.ToList());
+        }
+
+        // GET: Movements/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movements movements = db.Movements.Find(id);
+            if (movements == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movements);
+        }
+
+        // GET: Movements/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movements/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MovementId,MovementName")] Movements movements)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movements.Add(movements);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(movements);
+        }
+
+
+        // GET: Movements/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movements movements = db.Movements.Find(id);
+            if (movements == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movements);
+        }
+
+        // POST: Movements/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MovementId,MovementName")] Movements movements)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movements).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movements);
+        }
+
+        // GET: Movements/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movements movements = db.Movements.Find(id);
+            if (movements == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movements);
+        }
+
+        // POST: Movements/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Movements movements = db.Movements.Find(id);
+            db.Movements.Remove(movements);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
